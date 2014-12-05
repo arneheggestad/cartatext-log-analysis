85e79025f32a42b1f4356ba18953a6e30eca869e
Arne Heggestad <arne.heggestad@gmail.com>
2014-12-05 13:03:53 -0500

    initial skeleton

---
+++ bin/convoInfo.js
@@ -0,0 +1,8 @@
+var fs = require('fs')
+    ;
+
+var convoInfo = function (convos) {
+  var len = convos.length;
+};
+
+module.export = convoInfo;
--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 var userInfo = require('./bin/userInfo.js'),
+	convoInfo = require('./bin/convoInfo.js'),
 	fakeData = require('./bin/fakeData.js'),
 //	sampleUsers = require('./bin/sampleUsers.js'),
 	csv = require('csv-to-json')
 	;
 
+// Generate fake user log data
 //fakeData(1000);
 
-var fakeUsers = csv.parse('./data/2014.users.fake');
-userInfo(fakeUsers);
+// Process user log data
+//var fakeUsers = csv.parse('./data/2014.users.fake');
+//userInfo(fakeUsers);
 
-// var convos;
-// convoInfo(convos);
+// process convo log
+var fakeLog = csv.parse('./data/2014-12.log.fake');
+convoInfo(fakeLog);
+
