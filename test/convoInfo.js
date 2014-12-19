c54fac7b074cbfb5a556146c52724d76597d25af
Arne Heggestad <arne.heggestad@gmail.com>
2014-12-19 10:22:44 -0500

    added getTokens() method

--- test/convoInfo.js
+++ test/convoInfo.js
@@ -4,16 +4,36 @@ var should = require('should'),
 
 var convoArray = require('./data/2014-12-fakeLog.js'),
 		getConvoIDResult = [ 
-		'7b347fa9-b0eb-4ea5-a735-92e37f640f01',
-	  '1b7b542f-9dae-4525-9ff9-9c6e40b6adba',
-	  'c217cecc-ca58-4f7a-86ef-b9c54abf7179',
-	  '5dfc6500-0377-4aa0-95cf-d8835e6fd1b3',
-	  'cd1a5db8-a0b1-4265-b838-15ff1d5ef6fd',
-	  '1cde68a5-5273-4c93-b5e4-44020a437e8d',
-	  'a7073831-b8c3-4fd8-8609-c6ee5d9e95d4',
-	  '20d51062-dd00-418f-9045-ff6627d42079',
-	  '33bc1abb-b24e-4b28-8e03-d15a8f07d06a' ];
-
+			'7b347fa9-b0eb-4ea5-a735-92e37f640f01',
+		  '1b7b542f-9dae-4525-9ff9-9c6e40b6adba',
+		  'c217cecc-ca58-4f7a-86ef-b9c54abf7179',
+		  '5dfc6500-0377-4aa0-95cf-d8835e6fd1b3',
+		  'cd1a5db8-a0b1-4265-b838-15ff1d5ef6fd',
+		  '1cde68a5-5273-4c93-b5e4-44020a437e8d',
+		  'a7073831-b8c3-4fd8-8609-c6ee5d9e95d4',
+		  '20d51062-dd00-418f-9045-ff6627d42079',
+		  '33bc1abb-b24e-4b28-8e03-d15a8f07d06a' ],
+	  getTokensResult = {
+	  	'convoTokens': [
+		  	'convoID', 
+		  	'convoStart', 
+		  	'totalConvoLength', 
+		  	'reasonForConvo' ],
+		  'sessionTokens': [
+		  	'sessionID',
+		    'sessionStart',
+		    'sessionLength',
+		    'tries',
+		    'query',
+		    'response',
+		    'routeNo',
+		    'querySansBus',
+		    'definitiveStop',
+		    'matchedStops',
+		    'filteredStops',
+		    'culledStops',
+		    'duplicateStops' ]
+	  }
 
 describe('convo log testing', function () {
 	it('should return an array of convo IDs', function (done) {
@@ -22,9 +42,24 @@ describe('convo log testing', function () {
 			done();
 		});
 	});
-	it('should return an object of the conversations', function (done) {
-		convoInfo.getSessions(convoArray, getConvoIDResult, function (err, convosObj) {
+	it('should find tokens for a log', function (done) {
+		convoInfo.getTokens(convoArray, function (err, tokens) {
+			tokens.should.eql(getTokensResult);
 			done();
+		});
+	});
+	it('should return an object of the conversations', function (done) {
+		convoInfo.getSessions(convoArray, getConvoIDResult, getTokensResult, function (err, convosObj) {
+			done();
+		});
+	});
+	it('putting it all together', function (done) {
+		convoInfo.getConvoIDs(convoArray, function (err, foundConvos) {
+			convoInfo.getTokens(convoArray, function (err, foundTokens) {
+				convoInfo.getSessions(convoArray, foundConvos, foundTokens, function (err, convoObj) {
+					done();
+				})
+			})
 		})
 	})
-})
+});
