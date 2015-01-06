arneheggestad/cartatext-log-analysis

commit 6c8c8648d3d48d76fb24af033dfdbf4067e14058

renamed some returned values to make them more consistent

lib/userInfo.js
@@ -26,7 +26,7 @@ var userInfo = {
     userArray = userArray.sort(function (a, b) {
       return b.uses - a.uses;
     });
-    userObj.count = userArray.length;
+    userObj.userCount = userArray.length;
     userObj.maxUse = userArray[0].uses;
     userObj.minUse = userArray[userObj.count - 1].uses;
     userObj.sumUses = 0;
@@ -35,7 +35,7 @@ var userInfo = {
       userObj.sumUses += parseInt(userArray[i].uses);
     };
 
-    userObj.mean = userObj.sumUses / userObj.count;
+    userObj.meanUse = userObj.sumUses / userObj.count;
     var medianUser = Math.round((userObj.count - 1) / 2);
     userObj.medianUse = parseInt(userArray[medianUser].uses);
     userInfo.classifyUsage(userArray, function (err, usageArr) {

commit 8e88cba0c499494a9dd60f88fc4239cbdd247061

fixed bug caused by commit 6c8c864; this is why we test, self

lib/userInfo.js
@@ -28,15 +28,15 @@ var userInfo = {
     });
     userObj.userCount = userArray.length;
     userObj.maxUse = userArray[0].uses;
-    userObj.minUse = userArray[userObj.count - 1].uses;
+    userObj.minUse = userArray[userObj.userCount - 1].uses;
     userObj.sumUses = 0;
 
-    for (var i = 0; i < userObj.count; i++) {
+    for (var i = 0; i < userObj.userCount; i++) {
       userObj.sumUses += parseInt(userArray[i].uses);
     };
 
-    userObj.meanUse = userObj.sumUses / userObj.count;
-    var medianUser = Math.round((userObj.count - 1) / 2);
+    userObj.meanUse = userObj.sumUses / userObj.userCount;
+    var medianUser = Math.round((userObj.userCount - 1) / 2);
     userObj.medianUse = parseInt(userArray[medianUser].uses);
     userInfo.classifyUsage(userArray, function (err, usageArr) {
       if (err) { return cb(err, null); }